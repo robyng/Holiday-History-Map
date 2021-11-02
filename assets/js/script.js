996d6fc2e271679b28cfe4a784c98826ad5f209f
Sterling Williams <sterlingw6187@gmail.com>
2021-11-01 20:27:46 -0700

    js link to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,3 @@
-
-fetch('https://api.openweathermap.org/data/2.5/weather?q=portland&appid=f82224f6-9d92-48c9-9d36-426be23071ff')
-.then(function(res){return res.json()})
-//.then(function(data){console.log(data)})
-//.catch(function(err){console.log(err)})
-.then(function(weatherResponse){
-    var cityNameEl = document.querySelector(".city-name");
-    cityNameEl.textContent = weatherResponse.name;
-
-})
+fetch('https://date.nager.at/api/v3/publicholidays/2017/AT')
+  .then(response => response.json())
+  .then(data => console.log(data));
