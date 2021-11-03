2a6a3ee9177a12ff717bb0dd807b6d9aa1d502df
robyng <robyngraham01@gmail.com>
2021-11-03 12:08:51 -0700

    add resp-div and resp-iframe classes to make youtube videos in iframe responsive on mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,7 @@ var createVidCol = function(){
     videoListEl.textContent = data.items[0].snippet.title
     
     var iframeEl = document.querySelector('.iframe')
-    iframeEl.innerHTML+= `<iframe width="560" height="315" src="https://www.youtube.com/embed/jK-YFggBdGU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+    iframeEl.innerHTML+= `<iframe class="resp-iframe" src="https://www.youtube.com/embed/jK-YFggBdGU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
 }
 createVidCol()
     
