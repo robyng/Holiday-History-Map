ac9156abb6fb10f1e0e2d9eb58e13af585f2a3f0
robyng <robyngraham01@gmail.com>
2021-11-03 09:50:02 -0700

    added start and end comments to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+var axios = require("axios").default;
+
+var options = {
+  method: 'GET',
+  url: 'https://salnazi-country-codes-v1.p.rapidapi.com/api.php',
+  params: {apikey: '8923sa7aAS7s', secret: 'us7uS78sj', apiKey: '8923sa7aAS7s'},
+  headers: {
+    'x-rapidapi-host': 'salnazi-country-codes-v1.p.rapidapi.com',
+    'x-rapidapi-key': 'd35870a259mshb2f120193b48082p1e7145jsnb6dc59be0daa'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+})
 var mexicoBtn = document.getElementById("mexico").addEventListener("click", function(){
 
     console.log("mexico clicked")
