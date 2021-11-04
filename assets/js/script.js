63df2ded578e028882caf7b648ea7f721002c76c
robyng <robyngraham01@gmail.com>
2021-11-04 14:02:03 -0700

    create event for div with buttons, var for country code, adds country code to fetch url

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,11 @@
-
-var mexicoBtn = document.getElementById("mexico").addEventListener("click", function(){
-//mexico new years fetch
-    fetch("https://youtube-v31.p.rapidapi.com/search?q=mexico%20new%20years&part=snippet%2Cid&regionCode=US&maxResults=4&order=relevance", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "youtube-v31.p.rapidapi.com",
-		"x-rapidapi-key": "d35870a259mshb2f120193b48082p1e7145jsnb6dc59be0daa"
-	}
-})
-.then(response => {
-	{response.json()
-    .then(function(data) {
-    console.log(data)
+var countryList = document.querySelector(".country-list").addEventListener("click", function(event){
+    countryCodeId = event.target.id
+    console.log(countryCodeId)
 
 
-
-// end mexicao new years fetch
-
-// start creat column list for video content
-var createVidCol = function(){
-
-    var videoListEl = document.querySelector(".video-list")
-       
-    videoListEl.innerHTML+=`
-
-        <li><span class="video"></span> </li>
-        ` 
-    var videoListEl = document.querySelector('.video')
-    videoListEl.textContent = data.items[0].snippet.title
-
-    var videoId = data.items[0].id.videoId
+    // start public holiday fetch
     
-    var iframeEl = document.querySelector('.iframe')
-    iframeEl.innerHTML+= `<iframe class="resp-iframe" src="https://www.youtube.com/embed/${videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
-}
-createVidCol()
-    
-    })   
-    
-    }
-})
-   
-
-// start public holiday fetch
-    console.log("mexico clicked")
-    fetch("https://public-holiday.p.rapidapi.com/2022/MX", {
+    fetch("https://public-holiday.p.rapidapi.com/2022/" + countryCodeId, {
         "method": "GET",
         "headers": {
             "x-rapidapi-host": "public-holiday.p.rapidapi.com",
@@ -98,4 +59,58 @@ createVidCol()
         }
     })
 
+
+    
+
 })
+
+
+
+
+
+// var mexicoBtn = document.getElementById("mexico").addEventListener("click", function(){
+//     //mexico new years fetch
+//         fetch("https://youtube-v31.p.rapidapi.com/search?q=mexico%20new%20years&part=snippet%2Cid&regionCode=US&maxResults=4&order=relevance", {
+//         "method": "GET",
+//         "headers": {
+//             "x-rapidapi-host": "youtube-v31.p.rapidapi.com",
+//             "x-rapidapi-key": "d35870a259mshb2f120193b48082p1e7145jsnb6dc59be0daa"
+//         }
+//     })
+//     .then(response => {
+//         {response.json()
+//         .then(function(data) {
+//         console.log(data)
+
+
+
+//     // end mexicao new years fetch
+
+//     // start creat column list for video content
+//     var createVidCol = function(){
+
+//         var videoListEl = document.querySelector(".video-list")
+        
+//         videoListEl.innerHTML+=`
+
+//             <li><span class="video"></span> </li>
+//             ` 
+//         var videoListEl = document.querySelector('.video')
+//         videoListEl.textContent = data.items[0].snippet.title
+
+//         var videoId = data.items[0].id.videoId
+        
+//         var iframeEl = document.querySelector('.iframe')
+//         iframeEl.innerHTML+= `<iframe class="resp-iframe" src="https://www.youtube.com/embed/${videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+//     }
+//     createVidCol()
+        
+//         })   
+        
+//         }
+//     })
+   
+
+
+
+// })
